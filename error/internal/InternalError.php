0557740aaf11737455a6912f8ad137a4f9972c88
noliktop <aydansuper12@gmail.com>
2020-05-25 16:39:23 +0400

    Реализовал Internal ошибки

--- error/internal/InternalError.php
+++ error/internal/InternalError.php
@@ -4,7 +4,34 @@
 namespace SociallHouse\prismaFrame\error\internal;
 
 
-class InternalError
+final class InternalError
 {
+
+	/*
+	 * Internal ошибки проявляются только при запуске процесса, когда происходит валидация контроллеров и их методов
+	 * Во время работы процесса данные ошибки не появляются
+	 */
+
+	private function __construct(){}
+
+	public static function NO_DOC(): InternalErrorException{
+		return new InternalErrorException(InternalErrorCodes::NO_DOC, "Метод не содержит php-doc");
+	}
+
+	public static function WRONG_RETURN_TYPE(): InternalErrorException{
+		return new InternalErrorException(InternalErrorCodes::WRONG_RETURN_TYPE, "Метод должен возвращать тип array");
+	}
+
+	public static function NO_SUPPORT_HTTP_METHODS(): InternalErrorException{
+		return new InternalErrorException(InternalErrorCodes::NO_SUPPORT_HTTP_METHODS, "Php-doc метода должен содержать @method <GET|POST|PATCH|PUT>");
+	}
+
+	public static function NOT_ENOUGH_ARGS(): InternalErrorException{
+		return new InternalErrorException(InternalErrorCodes::NOT_ENOUGH_ARGS, "Php-doc метода содержит упоминание не всех аргументов функции");
+	}
+
+	public static function UNKNOWN_PARAMETER_TYPE(): InternalErrorException{
+		return new InternalErrorException(InternalErrorCodes::UNKNOWN_PARAMETER_TYPE, "Тип аргумента функции не является примитивным");
+	}
 
 }
--- error/internal/InternalErrorCodes.php
+++ error/internal/InternalErrorCodes.php
@@ -7,4 +7,10 @@ namespace SociallHouse\prismaFrame\error\internal;
 interface InternalErrorCodes
 {
 
+	const NO_DOC = 0x00; // Метод не содержит php-doс
+	const WRONG_RETURN_TYPE = 0x01; // Метод должен возвращать массив
+	const NO_SUPPORT_HTTP_METHODS = 0x02; // Php-doc метода не содержит информацию о том, какие http-методы будет он принимать
+	const NOT_ENOUGH_ARGS = 0x03; // Указаны не все аргументы функции в php-doc
+	const UNKNOWN_PARAMETER_TYPE = 0x0a;
+
 }
