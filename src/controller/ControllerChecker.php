cd0aef7d3223e71cc61d37278fdcf7543dd8ffe0
NolikTop <aydansuper12@gmail.com>
2023-05-03 01:51:00 +0300

    remove phpdoc parsing

--- src/controller/ControllerChecker.php
+++ src/controller/ControllerChecker.php
@@ -51,14 +51,7 @@ class ControllerChecker {
   protected function checkAndGetMethod(ReflectionMethod $method, Controller $controller): Method {
     $methodName = $method->getName();
 
-    $comment = $method->getDocComment();
-    if ($comment === false) {
-      $doc = [];
-    } else {
-      $doc = self::parseDoc($comment);
-    }
-
-    $this->checkReturnType($method, $doc);
+    $this->checkReturnType($method);
 
     $attrs = $method->getAttributes(HttpMethod::class);
     if (count($attrs) === 0) {
@@ -81,19 +74,17 @@ class ControllerChecker {
       }
     }
 
-    $parameters = $this->checkAndGetParameters($method, $doc);
+    $parameters = $this->checkAndGetParameters($method);
 
     return new Method($methodName, $parameters, $httpMethods, $controller);
   }
 
   /**
-   * @param (string|string[])[] $doc
    * @return MethodParameter[] array<string, MethodParameter>
    *
    * @throws InternalErrorException
    */
-  protected function checkAndGetParameters(ReflectionMethod $method, array $doc): array {
-    $docParameters = $this->getParametersFromDocArray($doc);
+  protected function checkAndGetParameters(ReflectionMethod $method): array {
     $resultParameters = [];
 
     $i = 0;
@@ -104,26 +95,18 @@ class ControllerChecker {
       }
 
       $parameterName = $methodParameter->getName();
-      if (isset($docParameters[$i])) {
-        $docParameter = $docParameters[$i];
-        if ($docParameter->name !== $parameterName) {
-          throw new InternalErrorException("The order of the arguments in PHPDoc not match the order of the method arguments");
-        }
 
-        $docParameter->required = !$methodParameter->isOptional();
-      } else {
-        $extraData = [];
-        $docParameter = new MethodParameter(
-          $this->prismaFrame->getTypeManager(),
-          $parameterName,
-          $type->getName(),
-          $extraData,
-          !$methodParameter->isOptional()
-        );
-      }
+      $extraData = [];
+      $parameter = new MethodParameter(
+        $this->prismaFrame->getTypeManager(),
+        $parameterName,
+        $type->getName(),
+        $extraData,
+        !$methodParameter->isOptional()
+      );
 
-      $resultParameters[$parameterName] = $docParameter;
-      $this->checkParameterType($docParameter);
+      $resultParameters[$parameterName] = $parameter;
+      $this->checkParameterType($parameter);
 
       ++$i;
     }
@@ -146,10 +129,9 @@ class ControllerChecker {
   }
 
   /**
-   * @param (string|string[])[] $doc
    * @throws InternalErrorException
    */
-  protected function checkReturnType(ReflectionMethod $method, array $doc): void {
+  protected function checkReturnType(ReflectionMethod $method): void {
     $returnType = $method->getReturnType();
 
     $requiredReturnType = 'array';
@@ -162,104 +144,6 @@ class ControllerChecker {
     if ($actualReturnTypeName !== $requiredReturnType) {
       throw new InternalErrorException("Method returns $actualReturnTypeName instead of $requiredReturnType");
     }
-
-    if (!isset($doc['return'])) {
-      // все ок, точно возвращаем массив
-      return;
-    }
-
-    $docReturnType = $doc['return'][0];
-    if ($docReturnType !== $requiredReturnType) {
-      throw new InternalErrorException("The @return in PHPDoc refers to $docReturnType instead of $requiredReturnType");
-    }
-  }
-
-  /**
-   * @return (string|string[])[]
-   */
-  protected function parseDoc(string $data): array {
-    $result = [];
-    $lines = explode("\n", $data);
-
-    foreach ($lines as $line) {
-      $line = trim($line);
-
-      /*
-       * проверяется сценарий такой же как в этом комментарии
-       * @parameter value
-       */
-      $parameterPrefix = "* @"; // первые 3 символа строки с параметром
-      $isLineWithParameter = substr($line, 0, 3) === $parameterPrefix;
-
-      if (!$isLineWithParameter) {
-        continue;
-      }
-
-      $tokens = explode(' ', $line);
-      array_shift($tokens); //Избавляемся от '*' в начале
-
-      $parameterNameWithAmpersat = array_shift($tokens); // ampersat - @
-      $parameterName = substr($parameterNameWithAmpersat, 1);
-
-      if ($this->isArrayParameter($parameterName)) {
-        $result[$parameterName][] = $tokens;
-      } else {
-        $result[$parameterName] = $tokens;
-      }
-    }
-
-    return $result;
-  }
-
-  /**
-   * @param (string|string[])[] $doc
-   * @return MethodParameter[]
-   * @throws InternalErrorException
-   */
-  private function getParametersFromDocArray(array $doc): array {
-    $result = [];
-
-    foreach ($doc['param'] ?? [] as $param) {
-      /* просто напомню как в доке это лежит
-       * @param type $var some extra data
-       *
-       * соответственно в $param будет
-       * ["type", "$var", "some", "extra", "data"]
-       */
-
-      if (!isset($param[0], $param[1])) {
-        throw new InternalErrorException('Wrong @param line');
-      }
-
-      $typeName = array_shift($param);
-      if (str_contains($typeName, '|')) {
-        throw new InternalErrorException("Multiple types for parameter are not supported");
-      }
-
-      $parameterName = array_shift($param);
-
-      if ($parameterName[0] !== "$") {
-        throw new InternalErrorException("@param \"{$parameterName}\" has bad name (without '$')");
-      }
-
-      if (isset($result[$parameterName])) {
-        throw new InternalErrorException("Duplicate @param {$parameterName}");
-      }
-
-      $extraData = $param;
-
-      $parameterName = substr($parameterName, 1); // убираем $
-
-      $result[] = new MethodParameter($this->prismaFrame->getTypeManager(), $parameterName, $typeName, $extraData, false);
-    }
-
-    return $result;
-  }
-
-  public function isArrayParameter(string $parameterName): bool {
-    $arrayParameters = ['param', 'throws'];
-
-    return in_array($parameterName, $arrayParameters, true);
   }
 
   public function isMethodInternal(string $methodName): bool {
