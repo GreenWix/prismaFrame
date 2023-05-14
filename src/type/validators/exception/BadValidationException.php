7eb6ec3efd73f5c0e341c6ed39efdc2d41e104f8
NolikTop <aydansuper12@gmail.com>
2023-05-15 00:45:33 +0300

    change http code

--- src/type/validators/exception/BadValidationException.php
+++ src/type/validators/exception/BadValidationException.php
@@ -12,7 +12,7 @@ class BadValidationException extends ValidatorException {
 
   const DEFAULT_MESSAGE = "Wrong input";
 
-  public function __construct(string $message = self::DEFAULT_MESSAGE, int $httpCode = HTTPCodes::INTERNAL_SERVER_ERROR, Exception $previous = null) {
+  public function __construct(string $message = self::DEFAULT_MESSAGE, int $httpCode = HTTPCodes::BAD_REQUEST, Exception $previous = null) {
     parent::__construct(ErrorCodes::BAD_VALIDATION_RESULT, $message, $httpCode, $previous);
   }
 
